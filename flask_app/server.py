b82fb6e786cafb34740054f04b1eb98281aac6eb
Sandro Young <ord-nas@users.noreply.github.com>
2020-04-30 22:49:26 -0700

    Test server code to accept exported frames.
    Still nothing on the client side that exercises this, but you can test
    from the developer console.
    
    Select the canvas element to export, and then do:
    
    $.post("../start_export",
      {
        filename: "test.avi",
        fps: "24",
        frame_width: "1280",
        frame_height: "720",
      },
      function(data, status){
        console.log(data);
        console.log(status);
      });
    
    $.post("../write_frame",
      {
        data_url: $0.toDataURL()
      },
      function(data, status){
        console.log(data);
        console.log(status);
      });
    
    $.post("../finish_export",
      {
      },
      function(data, status){
        console.log(data);
        console.log(status);
      });
    
    You can hit the write_frame endpoint many times to write more than one
    frame.
    
    Also code is gross and needs to be cleaned up to add more options and
    error handling.

--- flask_app/server.py
+++ flask_app/server.py
@@ -1,9 +1,92 @@
-from flask import Flask
+import os
+import base64
+import cv2
+import numpy as np
+from flask import Flask, request
 app = Flask(__name__,
             static_url_path='/static/',
             static_folder='static')
 
+class ExportManager(object):
+    def __init__(self, export_dir):
+        self.export_dir = export_dir
+        self.video_writer = None
+    def start_export(self, filename, fps, frame_width, frame_height):
+        try:
+            if self.video_writer is not None:
+                self.video_writer.release()
+            path = os.path.join(self.export_dir, filename)
+            self.video_writer = cv2.VideoWriter(
+                path,
+                cv2.VideoWriter_fourcc('M','J','P','G'),
+                fps,
+                (frame_width,frame_height))
+            return True
+        except:
+            self.video_writer = None
+            return False
+    def write_frame(self, frame):
+        try:
+            self.video_writer.write(frame)
+            return True
+        except:
+            return False
+    def finish_export(self):
+        try:
+            self.video_writer.release()
+            self.video_writer = None
+            return True
+        except:
+            return False
+
+# Global singleton
+export_manager = ExportManager('/home/sandro/Documents/Canvas Exports/')
+
+def decode_frame(data_url):
+    try:
+        header = "data:image/png;base64,"
+        if not data_url.startswith(header):
+            return None
+        encoded_data = data_url[len(header):]
+        data = base64.b64decode(encoded_data)
+        arr = np.fromstring(data, np.uint8)
+        frame = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+        return frame
+    except:
+        return None
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test! How is it going?'
+    return 'Hello, World!'
+
+@app.route('/start_export', methods=['POST'])
+def start_export():
+    global export_manager
+    success = export_manager.start_export(request.form['filename'],
+                                          int(request.form['fps']),
+                                          int(request.form['frame_width']),
+                                          int(request.form['frame_height']))
+    error_code = 200 if success else 500
+    return '', error_code
+
+@app.route('/write_frame', methods=['POST'])
+def write_frame():
+    global export_manager
+
+    frame = decode_frame(request.form['data_url'])
+    if frame is None:
+        return '', 500
+
+    success = export_manager.write_frame(frame)
+    error_code = 200 if success else 500
+    return '', error_code
+
+@app.route('/finish_export', methods=['POST'])
+def finish_export():
+    global export_manager
+
+    success = export_manager.finish_export()
+    error_code = 200 if success else 500
+    return '', error_code
+
 
